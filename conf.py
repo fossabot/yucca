56b6801f9000c51a4ef37f5e7fa6c6f088349403
afranceschetti <alessandro.franceschetti@csi.it>
2019-02-11 12:55:40 +0100

    test

--- conf.py
+++ conf.py
@@ -76,8 +76,10 @@ pygments_style = None
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
-html_theme_path = ["docs/_themes", ]
+html_theme = 'sphinx_rtd_theme'
+# html_theme_path = ["_themes", ]
+#html_static_path = ['_static']
+#html_theme_path = ["docs/_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -89,6 +91,9 @@ html_theme_path = ["docs/_themes", ]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+def setup(app):
+    # app.add_stylesheet("css/custom.css")
+    app.add_stylesheet("yucca.css")
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
