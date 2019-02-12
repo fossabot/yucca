85ee2f2dcd1ccf074149e49123eb3b55227c19ca
afranceschetti <alessandro.franceschetti@csi.it>
2019-02-12 17:54:47 +0100

    layout1

--- _static/js/yucca.js
+++ _static/js/yucca.js
@@ -4,7 +4,10 @@ $(document).ready(function(){
 		dataType:'jsonp',
 		jsonp: "callback",
 		success : function(data) {              
-			console.log('info: '+data);
+			console.log('info : ', data);
+			if(data && data.user && data.user.username && data.user.username!=null)){
+				$("#username").html('<i class="fa fa-user"></i> ' + data.user.username);
+			}
 		},
 		error : function(request,error){
 			console.log("Error info: "+JSON.stringify(request));
