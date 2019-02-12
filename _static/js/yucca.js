615250e23bbf88f2f107efbd866b14c5a93cfb6a
afranceschetti <alessandro.franceschetti@csi.it>
2019-02-12 18:27:51 +0100

    footer

--- _static/js/yucca.js
+++ _static/js/yucca.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 		success : function(data) {              
 			console.log('info : ', data);
 			if(data && data.user && data.user.username && data.user.username!=null){
-				$("#username").html('<i class="fa fa-user"></i> ' + data.user.username);
+				$("#username").html('<i class="fa-user-circle fa-lg"></i>&nbsp;' + data.user.username);
 			}
 		},
 		error : function(request,error){
